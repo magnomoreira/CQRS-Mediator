d0f8b69e372024f2cf6d8eb6911359ec907bb351
magno Moreira <magnomoreira@yahoo.com>
2021-08-13 16:40:10 -0300

    Criando pasta e classe Handle

---
+++ PadraoCQRS/Domain/Handler/CreateCustumerHandler.cs
@@ -0,0 +1,29 @@
+﻿using PadraoCQRS.Domain.Commands.Request;
+using PadraoCQRS.Domain.Commands.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PadraoCQRS.Domain.Handler
+{
+	public class CreateCustumerHandler
+	{
+		public CreateCustumerResponse Handle(CreateCustumerRequest)
+		{
+			// Verificar se o cliente ja esta cadastrado
+			// validar os dados
+			// Inserir um cliente
+			// Enviar email de boas vindas
+
+			return new CreateCustumerResponse
+			{
+				Id = Guid.NewGuid(),
+				Name = "MAgno Moreira",
+				Email = "Magno@yahoo.com",
+				Date = DateTime.Now
+
+			};
+		}
+	}
+}
