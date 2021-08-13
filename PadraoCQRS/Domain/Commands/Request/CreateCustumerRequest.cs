0bf08f688deb242ea93450de11a5838f6c776afd
Magno Moreira <magnomoreira@yahoo.com>
2021-08-13 16:22:02 -0300

    Criação das pastas e classe Request e Response

---
+++ PadraoCQRS/Domain/Commands/Request/CreateCustumerRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace PadraoCQRS.Domain.Commands.Request
+{
+	// Requisição do cliente 
+	public class CreateCustumerRequest
+	{
+		public string  Name { get; set; }
+		public string Email { get; set; }
+
+	}
+}
---
+++ PadraoCQRS/Domain/Commands/Response/CreateCustumerResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+// Resposta para o cliente 
+namespace PadraoCQRS.Domain.Commands.Response
+{
+	public class CreateCustumerResponse
+	{
+		public Guid Id { get; set; }
+		public string Name { get; set; }
+		public string Email { get; set; }
+		public DateTime Date { get; set; }
+
+	}
+}
