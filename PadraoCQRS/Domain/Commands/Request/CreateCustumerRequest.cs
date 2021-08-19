f03a2e8fadfb57620e13c0a7960cef00951b7223
magno Moreira <magnomoreira@yahoo.com>
2021-08-19 11:25:23 -0300

    Criando a classe custumer e refatorando o codigo

--- PadraoCQRS/Domain/Commands/Request/CreateCustumerRequest.cs
+++ PadraoCQRS/Domain/Commands/Request/CreateCustumerRequest.cs
@@ -6,7 +6,8 @@ namespace PadraoCQRS.Domain.Commands.Request
 	// Requisição do cliente 
 	public class CreateCustumerRequest : IRequest<CreateCustumerResponse>
 	{
-		public string  Name { get; set; }
+		public string  FirstName { get; set; }
+		public string LastName { get; set; }
 		public string Email { get; set; }
 
 	}
