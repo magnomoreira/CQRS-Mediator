88de9d678bef6850de823549efc1d9b5d5e01a83
magno Moreira <magnomoreira@yahoo.com>
2021-08-22 19:29:19 -0300

    Creating class AplicationContext

---
+++ PadraoCQRS/Data/AplicationContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PadraoCQRS.Domain;
+
+namespace PadraoCQRS.Data
+{
+	public class AplicationContext : DbContext
+	{
+
+		public DbSet<Custumer> Custumers { get; set; }
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsbuilder)
+		{
+			optionsbuilder.UseSqlServer("");
+		}
+
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			builder.ApplyConfigurationsFromAssembly(typeof(AplicationContext).Assembly);
+		}
+	}
+}
