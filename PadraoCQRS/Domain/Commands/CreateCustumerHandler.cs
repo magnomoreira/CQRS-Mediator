67087cc1dc19528b557fee0de323217595c98215
magno Moreira <magnomoreira@yahoo.com>
2021-08-22 20:04:55 -0300

    Refatoring

--- PadraoCQRS/Domain/Commands/CreateCustumerHandler.cs
+++ PadraoCQRS/Domain/Commands/CreateCustumerHandler.cs
@@ -1,9 +1,5 @@
-﻿using PadraoCQRS.Domain.Commands.Request;
-using PadraoCQRS.Domain.Commands.Response;
+﻿using PadraoCQRS.Domain.Commands.Response;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace PadraoCQRS.Domain.Commands
 {
