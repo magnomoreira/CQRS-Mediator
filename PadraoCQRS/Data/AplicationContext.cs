6c08dc8c9d53bb5f570f8f36ba886039aa29abf8
Magno <42675117+magnomoreira@users.noreply.github.com>
2021-08-24 11:04:52 -0300

    Refatoring

--- PadraoCQRS/Data/AplicationContext.cs
+++ PadraoCQRS/Data/AplicationContext.cs
@@ -5,11 +5,9 @@ namespace PadraoCQRS.Data
 {
 	public class AplicationContext : DbContext
 	{
-
+		public DbSet<Custumer> Custumers {get; set;}
 		public AplicationContext() { }
 		public AplicationContext(DbContextOptions<AplicationContext> options) : base(options) { }
-
-		public DbSet<Custumer> Custumers { get; set; }
 
 		protected override void OnModelCreating(ModelBuilder builder)
 		{
